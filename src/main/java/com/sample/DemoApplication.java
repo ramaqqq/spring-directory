5a54a1f0329a807b31b11d0467c3e3dd744ef318
eeq <eeq@192.168.10.120>
2022-12-06 09:00:32 +0700

    first sql insert

--- src/main/java/com/sample/DemoApplication.java
+++ src/main/java/com/sample/DemoApplication.java
@@ -1,13 +1,12 @@
-package com.sample.demo;
-
+package com.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
--- src/test/java/com/sample/DemoApplicationTests.java
+++ src/test/java/com/sample/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sample.demo;
+package com.sample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
